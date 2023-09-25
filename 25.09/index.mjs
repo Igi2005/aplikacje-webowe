c50577cfa27c65033c08ce58d59cce870dee31f5
Igi2005 <118428435+Igi2005@users.noreply.github.com>
2023-09-25 14:01:19 +0200

    Zadanie z lekcji

---
+++ 25.09/index.mjs
@@ -0,0 +1,24 @@
+import * as http from 'http'
+import {readFile} from 'fs/promises'
+const  port = 3000
+const host = '127.0.0.1'
+
+const server = http.createServer(async(req,res)=> {
+    const url = req.url;
+
+    if(url == '/') {
+        res.statusCode = 200
+
+        const html = await readFile('index.html')
+
+        res.setHeader('content-type','text/html')
+
+        res.write(html)
+        res.end()
+    }
+
+})
+
+server.listen(port, host, () => {
+    console.log("Listening...");
+})
